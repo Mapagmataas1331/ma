d75088a417d950131cc0b7989d60fa142464522e
Mapagmataas <mapagmataas@outlook.com>
2023-04-10 17:08:27 +0700

    1331

--- src/main.js
+++ src/main.js
@@ -5,9 +5,20 @@ function addEventListenerList(list, event, fn) {
 }
 
 const header_page = document.getElementById("current-page");
-const page = document.location.pathname.split("/").slice(-1);
+var page = document.location.pathname.split("/").slice(-1);
+if (page == "index.html") { page = "in";
+} else if (page = "account.html") { page = "acc";
+} else page = page.slice(0, -5);
+document.getElementById("title").innerHTML = "ma." + page;
+header_page.innerHTML = page;
+document.getElementById(page).style.backgroundColor = "var(--third-color)";
 
 // navigation
 addEventListenerList(document.querySelectorAll(".nav-item"), "mouseover", (e) => {
-  header_page.innerHTML = e.target.id;
+  header_page.innerHTML = e.target.parentElement.id;
+  document.getElementById(page).style = null;
 });
+document.getElementById("navigation").addEventListener("mouseleave", () => {
+  header_page.innerHTML = page;
+  document.getElementById(page).style.backgroundColor = "var(--third-color)";
+}, false);
