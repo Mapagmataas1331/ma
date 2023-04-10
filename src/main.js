9373346b42b2e46f765a2f2827c16ae78a4bec7a
Mapagmataas <mapagmataas@outlook.com>
2023-04-10 17:29:01 +0700

    1331

--- src/main.js
+++ src/main.js
@@ -1,15 +1,24 @@
 function addEventListenerList(list, event, fn) {
   for (var i = 0, len = list.length; i < len; i++) {
       list[i].addEventListener(event, fn, false);
+}}
+function hrefTo(page) {
+  for (let i = 0; i <= 100; i++) {
+    document.getElementById("content").style.left = -i+"%"
   }
+  setTimeout(() => {
+    if (typeof page != undefined && page != null && page != "/index") {
+      location.href = page;
+    } else location.href = "/";
+  }, 500);
 }
 
-const header_page = document.getElementById("current-page");
 var page = document.location.pathname.split("/").slice(-1);
 if (page == "index.html") { page = "in";
 } else if (page = "account.html") { page = "acc";
 } else page = page.slice(0, -5);
 document.getElementById("title").innerHTML = "ma." + page;
+const header_page = document.getElementById("current-page");
 header_page.innerHTML = page;
 document.getElementById(page).style.backgroundColor = "var(--third-color)";
 
@@ -22,3 +31,6 @@ document.getElementById("navigation").addEventListener("mouseleave", () => {
   header_page.innerHTML = page;
   document.getElementById(page).style.backgroundColor = "var(--third-color)";
 }, false);
+addEventListenerList(document.querySelectorAll(".nav-item"), "click", (e) => {
+  hrefTo("/" + e.target.parentElement.firstElementChild.innerHTML);
+});
