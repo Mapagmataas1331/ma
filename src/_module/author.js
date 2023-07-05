e309653ba958df910f658b8d48fb420352379132
Mapagmataas <mapagmataas@outlook.com>
2023-07-05 22:08:34 +0700

    1331

--- src/_module/author.js
+++ src/_module/author.js
@@ -1,8 +1,8 @@
 trans_arr.push(
-  "Name: Anashin Timofey", "Имя: Анашин Тимофей",
-  "Username: Mapagmataas", "Никнейм: Mapagmataas",
-  "Age: 19 (12.13.2003)", "Возраст: 19 (13.12.2003)",
-  "City: Russia, Novosibirsk", "Город: Новосибирск",
+  "Name: <b>Anashin Timofey</b>", "Имя: <b>Анашин Тимофей</b>",
+  "Username: <b>Mapagmataas</b>", "Никнейм: <b>Mapagmataas</b>",
+  "Age: <b>19</b> (12.13.2003)", "Возраст: <b>19</b> (13.12.2003)",
+  "City: Russia, <b>Novosibirsk</b>", "Город: <b>Новосибирск</b>",
   "en", "ру",
   "en", "ру",
   "en", "ру",
