56c8614faadbb1b3a628bf41034c5a83cb762536
Mapagmataas <mapagmataas@outlook.com>
2023-07-05 21:58:10 +0700

    1331

--- src/_module/author.js
+++ src/_module/author.js
@@ -1,4 +1,12 @@
 trans_arr.push(
+  "Name: Anashin Timofey", "Имя: Анашин Тимофей",
+  "Username: Mapagmataas", "Никнейм: Mapagmataas",
+  "Age: 19 (12.13.2003)", "Возраст: 19 (13.12.2003)",
+  "City: Russia, Novosibirsk", "Город: Новосибирск",
+  "en", "ру",
+  "en", "ру",
+  "en", "ру",
+  "en", "ру",
   "en", "ру",
   "en", "ру",
   "en", "ру",
