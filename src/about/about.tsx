4409289531fac531179459a6e6897d050b777921
Mapagmataas <mapagmataas@outlook.com>
2023-11-02 18:11:59 +0700

    1331

---
+++ src/about/about.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './adout.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <h1>ABOUT PAGE</h1>
+  </React.StrictMode>,
+)
